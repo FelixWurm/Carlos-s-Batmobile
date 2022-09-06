075d8858a9fbebf8e6f3552d989456c98dc23d8e
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-06 16:26:04 +0200

    batman gateway

--- install-batman-gateway.sh
+++ install-batman-gateway.sh
@@ -11,11 +11,11 @@ sudo batctl if add wlan0
 sudo batctl gw_mode server
 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i bat0 -o eth0 -j ACCEPT
 
 # Activate interfaces
-sudo ifconfig wlan1 up
+sudo ifconfig wlan0 up
 sudo ifconfig bat0 up
 " | tee -a /home/pi/start-batman-adv.sh
