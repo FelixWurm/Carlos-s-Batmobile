33f74c3e030f5c229c922cf57acadd7784651e1c
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-08 15:15:34 +0200

    ux

--- network-test.sh
+++ network-test.sh
@@ -6,17 +6,16 @@ do
         t) PAUSETIME=${OPTARG};;
     esac
 done
+clear
+
 
 
 echo batctl throughputmeter to gateway:
 sudo batctl throughputmeter b8:27:eb:91:80:1b
-
 sleep $PAUSETIME
 echo;echo;echo
+
 
 echo speedtest-cli:
 speedtest
-
-sleep $PAUSETIME
-echo;echo;echo
 
