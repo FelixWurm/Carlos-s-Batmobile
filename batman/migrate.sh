63c32b00cbea80f24f68ec614c137e6bb3e8ad9a
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-12 16:11:14 +0200

    confirmed

--- batman/migrate.sh
+++ batman/migrate.sh
@@ -18,7 +18,7 @@ sudo rm -f /etc/rc.local
 sudo touch /etc/rc.local
 sudo chmod +x /etc/rc.local
 echo "#!/bin/sh -e
-$(pwd)/start-batman-adv.sh
+/home/pi/start-batman-adv.sh
 exit 0
 " | sudo tee -a /etc/rc.local
 
