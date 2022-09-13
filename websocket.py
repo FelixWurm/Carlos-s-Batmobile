a878524ffcf98b1a3ae45dd3a8af7fcabc0da6f5
koplas <54645365+koplas@users.noreply.github.com>
2022-09-13 16:39:37 +0200

    Update websocket

--- websocket.py
+++ websocket.py
@@ -24,6 +24,10 @@ SPEED = 100
 async def carlos_controller():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    request = struct.pack("!B", int(4))
+    # Todo: Wait for connection accept
+    sock.sendto(request, ("192.168.199.34", 50000))
+
     while True:
         try:
             await asyncio.wait_for(UPDATE_EVENT.wait(), timeout=0.2)
@@ -32,37 +36,42 @@ async def carlos_controller():
             pass
         data = []
         if STATE == State.FORWARD:
-            data = struct.pack("!Bf", int(1), 100)
+            data = struct.pack("!Bf", int(1), SPEED)
         if STATE == State.BACKWARDS:
-            data = struct.pack("!Bf", int(1), -100)
+            data = struct.pack("!Bf", int(1), -SPEED)
         if STATE == State.KEEP_ALIVE:
             data = struct.pack("!B", int(6))
         if STATE == State.ROTATE_RIGHT:
-            data = struct.pack("!Bf", int(2), 100)
+            data = struct.pack("!Bf", int(2), SPEED)
         if STATE == State.ROTATE_LEFT:
-            data = struct.pack("!Bf", int(2), -100)
+            data = struct.pack("!Bf", int(2), -SPEED)
 
-        sock.sendto(data, ("192.168.199.24", 22))
+        sock.sendto(data, ("192.168.199.34", 50000))
         print(STATE)
+        print(SPEED)
 
 
 async def handler(websocket):
     global STATE
+    global SPEED
+
     async for message in websocket:
         event = json.loads(message)
-        assert event["type"] == "keyPress"
-        key = event["key"]
+        if event["type"] == "keyPress":
+            key = event["key"]
 
-        if key == 'a':
-            STATE = State.ROTATE_LEFT
-        if key == 'w':
-            STATE = State.FORWARD
-        if key == 's':
-            STATE = State.BACKWARDS
-        if key == 'd':
-            STATE = State.ROTATE_RIGHT
-        if key == 'no_key':
-            STATE = State.KEEP_ALIVE
+            if key == 'a':
+                STATE = State.ROTATE_LEFT
+            if key == 'w':
+                STATE = State.FORWARD
+            if key == 's':
+                STATE = State.BACKWARDS
+            if key == 'd':
+                STATE = State.ROTATE_RIGHT
+            if key == 'no_key':
+                STATE = State.KEEP_ALIVE
+        if event["type"] == "speed":
+            SPEED = event["speed"]
 
         UPDATE_EVENT.set()
 
