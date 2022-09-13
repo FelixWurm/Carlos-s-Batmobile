8ce135a5886cccb463b7211f7306667bf5755701
FelixWurm <77516863+FelixWurm@users.noreply.github.com>
2022-09-13 16:00:05 +0200

    Update msg_dict.py

--- controllsoftware/msg_dict.py
+++ controllsoftware/msg_dict.py
@@ -5,5 +5,8 @@ msg_dict = {
     "DV_RAW_MODE"   : int(3),       #Float(motor L), Float(motor R)
     "CONN_REQUEST"  : int(4),       #Request a conecction, No additional value
     "CONN_ACCEPT"   : int(5),       #Accept a connection, NO additional value
-    "DV_STOP"       : int(6)
+    "DV_STOP"       : int(6),       #no additional value
+    "ACK"           : int(7)        #singal that something was recived
 }
+
+#in order to connect to the server send a CONN_REQUEST and wait for a CONN_ACCEPT.
