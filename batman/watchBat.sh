20088fd2f2e6b305a0e6aab0db21350318c9b786
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-14 09:22:55 +0200

    headers are back

--- batman/watchBat.sh
+++ batman/watchBat.sh
@@ -2,8 +2,8 @@ watch -n .1 '
 echo Gateways
 sudo batctl gwl;
 echo;echo;echo;echo Originators / Next hop;
-sudo batctl o  -H | grep -ie BATMAN -ie last-seen -ie \*;
+sudo batctl o | grep -ie BATMAN -ie last-seen -ie \*;
 echo;echo;echo;echo IPV4 translation;
-sudo batctl dc -H
+sudo batctl dc
 
 '
