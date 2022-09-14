fa7f96752047248233cebf2d5dad11bb1c88f54f
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-14 09:16:46 +0200

    ipv4 translation

--- batman/watchBat.sh
+++ batman/watchBat.sh
@@ -1,5 +1,9 @@
 watch -n .1 '
+echo Gateways
 sudo batctl gwl;
-echo;echo;echo;
-sudo batctl o | grep -ie BATMAN -ie last-seen -ie \*
+echo;echo;echo;echo Originators / Next hop;
+sudo batctl o | grep -ie BATMAN -ie last-seen -ie \*;
+echo;echo;echo;echo IPV4 translation;
+sudo batctl dc
+
 '
