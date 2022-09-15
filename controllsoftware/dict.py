0cdff6a28cb630c1f91ca29f1904a60e15eaff3d
FelixWurm <77516863+FelixWurm@users.noreply.github.com>
2022-09-15 16:27:12 +0200

    Update dict.py

--- controllsoftware/dict.py
+++ controllsoftware/dict.py
@@ -1,15 +1,17 @@
 msg_dict = {
-    "STAY_ALIVE"   : int(0),        #no additional value 
-    "DV_STRAIGHT"   : int(1),       #Float(speed -100 - 100)
-    "DV_ROTATE"     : int(2),       #Float(speed -1ßß - 100)
-    "DV_RAW_MODE"   : int(3),       #Float(motor L), Float(motor R)
-    "CONN_REQUEST"  : int(4),       #Request a connection, No additional value
-    "CONN_ACCEPT"   : int(5),       #Accept a connection, NO additional value
-    "DV_STOP"       : int(6),       #no additional value
-    "ACK"           : int(7),       #signal that something was receive
-    "READY_CONN"    : int(9),       #ready for connection(UDP auto discovery)
-    "ERROR_CONN"    : int(10),      #send this signal ig there is in invalid Connection attempt
-    "NO_MODE"       : int(11)       #only for internal 
+    "STAY_ALIVE"        : int(0),        #no additional value 
+    "DV_STRAIGHT"       : int(1),       #Float(speed -100 - 100)
+    "DV_ROTATE"         : int(2),       #Float(speed -1ßß - 100)
+    "DV_RAW_MODE"       : int(3),       #Float(motor L), Float(motor R)
+    "CONN_REQUEST"      : int(4),       #Request a connection, No additional value
+    "CONN_ACCEPT"       : int(5),       #Accept a connection, NO additional value
+    "DV_STOP"           : int(6),       #no additional value
+    "ACK"               : int(7),       #signal that something was receive
+    "READY_CONN"        : int(9),       #ready for connection(UDP auto discovery)
+    "ERROR_CONN"        : int(10),      #send this signal ig there is in invalid Connection attempt
+    "NO_MODE"           : int(11),      #only for internal 
+    "DV_CALL_STRAIGHT"  : int(12),       #only for testing, first float speed , second time
+    "DV_CALL_ROTATE"    : int(13)       #only for testing, first float speed , second time
 
 }
 
