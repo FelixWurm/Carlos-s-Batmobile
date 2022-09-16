740be5c8349a3404674be0b63ff7a6e37495f9b9
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-16 16:13:00 +0200

    trace

---
+++ batman/trace.sh
@@ -0,0 +1,12 @@
+watch "
+echo;echo Carlos-I;
+sudo batctl tr b8:27:eb:04:0f:99;
+echo;echo Carlos-II;
+sudo batctl tr b8:27:eb:b3:65:d8;
+echo;echo Carlos-III;
+sudo batctl tr b8:27:eb:2b:d5:77;
+echo;echo Carlos-IV;
+sudo batctl tr b8:27:eb:aa:3c:be;
+echo;echo Carlos-V;
+sudo batctl tr b8:27:eb:a4:d5:1e;
+"
