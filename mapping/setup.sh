f545a331ba6f29a3028fc02bcdad6840c14ef231
dominik potulski <dpotulski@uni-usnabrueck.de>
2022-09-20 11:31:00 +0200

    mapping

--- mapping/setup.sh
+++ mapping/setup.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y bluetooth pi-bluetooth python3-pip bluez python-dev-is-python3 libbluetooth-dev pkg-config libboost-python-dev libboost-thread-dev libglib2.0-dev
-pip install numpy
+pip install numpy bluepy bt-proximity
