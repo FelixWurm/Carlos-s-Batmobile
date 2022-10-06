bcfcbbacb88d0ad1fa099c2852cbdad4b2f1b21e
lahellmann <82382269+lahellmann@users.noreply.github.com>
2022-10-06 12:20:24 +0200

    Update RPI_control.py

--- controllsoftware/RPI_control.py
+++ controllsoftware/RPI_control.py
@@ -307,7 +307,7 @@ def main():
             break
         except:
             datanumber += 1
-    laserdata.write("time, GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_ROT_X, GYRO_ROT_Y, MOUSE_X, MOUSE_Y, Laser_Distance\n")
+    laserdata.write("time, GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_ROT_X, GYRO_ROT_Y, MOUSE_X, MOUSE_Y, Laser_Distance, Speed_L, Speed_R\n")
 
     # Create a VL53L0X object
     try:
@@ -472,7 +472,7 @@ def main():
                         datanumber = datanumber + 1
                         #local save
                         laserdata = open(("LaserData" + str(datanumber) + ".csv"), "a")
-                        laserdata.write("time, GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_ROT_X, GYRO_ROT_Y, MOUSE_X, MOUSE_Y, Laser_Distance\n")
+                        laserdata.write("time, GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_ROT_X, GYRO_ROT_Y, MOUSE_X, MOUSE_Y, Laser_Distance, Speed_L, Speed_R\n")
                         soc.sendto(struct.pack("!Bi",dict.msg_dict["NEW_DATA_FILE_NR"],datanumber),ip_addr)
                         print("NEW FILE, File number :",datanumber)
                     if code == dict.msg_dict["DATA_PACKET_DISABLE"]:
