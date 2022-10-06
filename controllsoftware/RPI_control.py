62a4abe17db968a7211df86931077de7da7db296
lahellmann <82382269+lahellmann@users.noreply.github.com>
2022-10-06 11:03:19 +0200

    strings

--- controllsoftware/RPI_control.py
+++ controllsoftware/RPI_control.py
@@ -295,7 +295,7 @@ def main():
     count_loop = 0
     #file for laser data
     datanumber = 0
-    laserdata = open((LaserData + datanumber + ".csv"), "a")
+    laserdata = open(("LaserData" + datanumber + ".csv"), "a")
     laserdata.write("time,GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X,ACCEL_Y,ACCEL_Z GYRO_ROT_X, GYRO_ROT_Y, Laser_Distance\n")
 
     # Create a VL53L0X object
@@ -454,7 +454,7 @@ def main():
                         way = 0
                         laserdata.close()
                         datanumber = datanumber + 1
-                        laserdata = open((LaserData + datanumber + ".csv"), "a")
+                        laserdata = open(("LaserData" + datanumber + ".csv"), "a")
                         laserdata.write("time,GYRO_X, GYRO_Y, GYRO_Z, ACCEL_X,ACCEL_Y,ACCEL_Z GYRO_ROT_X, GYRO_ROT_Y, Laser_Distance\n")
 
                     if code == dict.msg_dict["DATA_PACKET_DISABLE"]:
